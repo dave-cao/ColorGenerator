dc7c5ed45fd92530264b0240d3cee41894e49f0c
David Cao <dacao@protonmail.com>
2023-01-27 00:32:27 -0700

    did another thing

--- image_handle.py
+++ image_handle.py
@@ -57,7 +57,6 @@ def get_colors(PIL_image):
     print("reading image")
     img = np.array(PIL_image)
     img = cv.resize(img, dim, interpolation=cv.INTER_AREA)
-    print(img.shape)
 
     print("reversing array")
     # img = cv.cvtColor(img, cv.COLOR_BGR2RGB)
